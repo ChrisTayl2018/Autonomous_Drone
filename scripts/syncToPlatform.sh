8aa23de35733b83ffb4c18232895fee6ec8215cc
ChrisTayl2018 <ctaylor1991@live.com.au>
2020-05-23 18:06:34 +1000

    ADD: Developed a application launch script to launch mavros, raspicam_node and hawkeye_sonar nodes. CHG: Relevant comments to the syncToPlatform.sh script
    Changes to be committed:
            modified:   scripts/syncToPlatform.sh
    	new file:   src/hawkeye_sonar/launch/application.launch
    	modified:   src/hawkeye_sonar/launch/sonar_array.launch

--- scripts/syncToPlatform.sh
+++ scripts/syncToPlatform.sh
@@ -6,10 +6,20 @@
 
 # IP Address of the target ECU (on-board drone raspberry pi in this project) 
 DRONE_IP='192.168.0.61'
+
 # Target user name of the ECU 
 DRONE_USER='dronobuntu_1604'
 
+# Define target platform
 TARGET=$DRONE_USER@$DRONE_IP:/home/$DRONE_USER/catkin_ws/src
 
 # List of directories/files that need to be synced to the target
+
+# Sonar sensor node 
 rsync -avz /home/$USER/catkin_ws/src/hawkeye_sonar $TARGET
+
+# Raspicam Node
+#rsync -avz /home/$USER/catkin_ws/src/raspicam_node $TARGET
+
+#Launch scripts to launch nodes 
+rsync -avz /home/$USER/catkin_ws/src/launch_all $TARGET
